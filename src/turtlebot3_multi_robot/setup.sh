beccecc73ff9248ab5c0c499b958e0beb2d29112
RoroHaq <safinhq13@gmail.com>
2025-05-05 12:51:29 -0400

    begin setup.sh

---
+++ src/turtlebot3_multi_robot/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script will be used for the basic setup thats needed, like moving the initial models around for 
+# the environment to work properly in your Humble/classic system.
+path="$HOME/.gazebo/models"
+
+echo $path
