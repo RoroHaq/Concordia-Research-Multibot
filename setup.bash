4fa83ff1f2f9357ff3c0fce6c33a530482a0daf8
RoroHaq <safinhq13@gmail.com>
2025-05-22 13:31:24 -0400

    fix script deleting main terminal when closing script

--- setup.bash
+++ setup.bash
@@ -3,7 +3,6 @@
 # This script will be used for the basic setup thats needed, like moving the initial models around for 
 # the environment to work properly in your Humble/classic system.
 path="$HOME/.gazebo/models"
-
 TITLE1="my_term_1_$$"
 TITLE2="my_term_2_$$"
 
@@ -19,8 +18,10 @@ echo "Model transfer done!"
 
 cleanup() {
     echo "Cleaning up..."
-    xdotool search --name "$TITLE1" windowkill 2>/dev/null
-    xdotool search --name "$TITLE2" windowkill 2>/dev/null
+    # xdotool search --name "$TITLE1" windowkill 2>/dev/null
+    # xdotool search --name "$TITLE2" windowkill 2>/dev/null
+    pkill -f "$TITLE1"
+    pkill -f "$TITLE2"
     echo "Script exiting..."
     exit 0
 }
@@ -28,11 +29,19 @@ cleanup() {
 trap cleanup SIGINT
 
 # Running the project
-gnome-terminal -- bash -c "export PROMPT_COMMAND=''; echo -ne '\033]0;$TITLE1\007'; colcon build2 >2&1 | sed 's/\x1b\].*[\aG]//g'; source install/setup.bash; ros2 launch turtlebot3_multi_robot gazebo_multi_empty_world.launch.py; exec bash" &
+gnome-terminal -- bash -c "
+        export PROMPT_COMMAND='';
+        echo -ne '\033]0;$TITLE1\007'; 
+        colcon build 2>&1 | sed 's/\x1b\].*[\aG]//g'; 
+        source install/setup.bash; ros2 launch turtlebot3_multi_robot gazebo_multi_empty_world.launch.py; 
+        exec bash" &
 
-gnome-terminal -- bash -c "export PROMPT_COMMAND=''; echo -ne '\033]0;$TITLE2\007'; colcon build 2>&1 | sed 's/\x1b\].*[\aG]//g'; source install/setup.bash; ros2 run turtlebot3_multi_robot goal_listener_and_sender.py; exec bash" &
-
-
+gnome-terminal -- bash -c "
+        export PROMPT_COMMAND='';
+        echo -ne '\033]0;$TITLE2\007'; 
+        colcon build 2>&1 | sed 's/\x1b\].*[\aG]//g';
+        source install/setup.bash; ros2 run turtlebot3_multi_robot goal_listener_and_sender.py; 
+        exec bash" &
 
 cd ../../
 
